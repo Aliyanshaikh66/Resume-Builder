7d9a7d045ed1ec3bb00738a9b1a9f89ce24c4ccc
Aliyan Shaikh <aliyanzahid02@gmail.com>
2024-11-06 15:26:32 +0500

    Feat Updated

---
+++ data/cvData.js
@@ -0,0 +1,142 @@
+export const cvData = {
+  name: "Aliyan Shaikh",
+  // image: "https://drive.google.com/file/d/1SZ3jsQ93lF1LAtcFhe0dxur1PXjGDAIv/view?usp=sharing",
+  jobTitle: "MERN Stack Developer",
+  location: "Karachi, Pakistan",
+  email: "aliyansh20@gmail.com",
+  linkedIn: "@aliyanshaikh66",
+  instagram: "@aliyanshaikh66",
+  facebook: "@aliyanshaikh66",
+  twitter: "@aliyanshaikh66",
+  github: "aliyanshaikh66",
+  website: "aliyanshaikh66.com",
+  about:
+    "After graduating from the programming department, I worked remotely on graphic design for about 5 years. I have been working to be a **Front End Developer** for the last 7 months and I am looking for a job in this field. Because of my old job, *I give importance to design and visuality in my work.*",
+
+  toolsAndTechSkills: [
+    "Photoshop",
+    "Final Cut Pro",
+    "Figma",
+    "Webflow",
+    "Sketch",
+    "Illustrator",
+  ],
+  industryKnowledge: [
+    "Javascript",
+    "TypeScript",
+    "React.js",
+    "Next.js",
+    "MongoDB",
+    "Node.js",
+    "Express.js",
+    "Python",
+    "Django",
+    "Tailwind",
+  ],
+//   English: Native or Bilingual
+// Hindi: Native or Bilingual
+// Panjabi, Punjabi: Native or Bilingual
+// Urdu: Native or Bilingual
+  languages: ["English", "(Native)"],
+  skillTitle1: "Tools and Tech Skills",
+  skillTitle2: "Industry Knowledge",
+  skillTitle3: "Languages",
+  projects: [
+    {
+      title: "CV Builder",
+      link: "#",
+      summary: `With this project you can create real time CV. You can see the changes you have made live. Built with Tailwind and contextAPI.`,
+    },
+    {
+      title: "Portfolio",
+      link: "#",
+      summary: `My personal website, that I created with Next.js, Tailwind Css, Content Layer.`,
+    },
+
+    {
+      title: "Weather App",
+      link: "#",
+      summary: `In this work, a small project where I visualize the data from the API.`,
+    },
+  ],
+  education: [
+    {
+      title: "Computer Science",
+      school: "The City Grammar School",
+      startDate: "2018",
+      endDate: "2020",
+    },
+    {
+      title: "Pre-Engineering",
+      school: "D.J Science College",
+      startDate: "2020",
+      endDate: "2022",
+    },
+  ],
+  experiences: [
+    {
+      title: "Graphic Designer, Data Analysis",
+      company: "JPMC",
+      startDate: "2016",
+      endDate: "2020",
+      current: true,
+      summary: ` My job task:
+       Preparation of up-to-date statistics according to the situation in the stock market. 
+       Preparing visual infographics about the obtained data and organizing these data..`,
+    },
+    {
+      title: "Senior Analyst",
+      company: "Amazon",
+      startDate: "2021",
+      endDate: "Present",
+      current: true,
+      summary: `My job task:
+      Leading a team of 5 colleagues, selcting and shortlisting statistics and infographics, analyzing data and presenting data weekly...`,
+    },
+  ],
+  displayImage: true,
+  displayMail: true,
+  displayWebSite: true,
+  displayLinkedIn: false,
+  displayInstagram: false,
+  displayFacebook: false,
+  displayGithub: true,
+  displayTwitter: true,
+  activeColor: "#5B21B6",
+};
+
+/*
+export const cvDataEmpty = {
+  name: "",
+  jobTitle: "",
+  location: "",
+  email: "",
+  linkedin: "",
+  twitter: "",
+  github: "",
+  website: "",
+  aboutme: "",
+  toolsAndTechSkills: [],
+  industryKnowledge: [],
+  languages: [],
+  projects: [
+    {
+      title: "",
+      summary: "",
+    },
+  ],
+  experience: [
+    {
+      title: "",
+      company: "",
+      from: new Date(),
+      to: new Date(),
+      current: true,
+      summary: "",
+    },
+  ],
+  displayEducation: false,
+  displayProjects: false,
+  activeColor: "#5B21B6",
+};
+*/
